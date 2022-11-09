0bad7d26557fa29881dd5dde13b00f9c243d1dd9
adam-loi <117617342+adam-loi@users.noreply.github.com>
2022-11-09 12:53:28 +0800

    Create Tree.java

---
+++ Tree.java
@@ -0,0 +1,14 @@
+// Define your tree class in this file!
+public class Tree {
+  public static void main(String[] args) {
+    // Program that prints a tree to the screen!
+    System.out.println("Hey there, I'm Adam and I'm learning to code in Java!");
+    System.out.println("Time to plant a tree!");
+
+    System.out.println("  *  ");
+    System.out.println(" *** ");
+    System.out.println(" *** ");
+    System.out.println("  *  ");
+    System.out.println("  *  ");
+  }
+}
